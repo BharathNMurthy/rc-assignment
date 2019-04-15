ed6fe55e0decd4bfb61982821dd062bbd6fd93c1
Bharath <bharathnmurthy134@gmail.com>
2019-04-15 14:31:00 +0530

    fixed command to run the build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 npm run install
-npm install build-react
+npm run build-react
 cp -r client/build/ docker/build/
 cp -r server/ docker/server/
 cd docker; docker build -t app . --no-cache
